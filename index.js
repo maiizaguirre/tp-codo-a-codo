ce3e55cb8b9154833d88e3c58143026bd7efb7dc
Maximiliano Gasquez <maxigasquez@gmail.com>
2022-10-18 13:20:38 -0300

    Modificacion pestaña eventos, funcionalidad API

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
 const navToggle = document.querySelector(".nav-toggle");
 const navMenu = document.querySelector(".list-menu");
 
+//variables
+const url = 'https://jsonplaceholder.typicode.com/users';
+const respuesta = document.querySelector("#respuesta");
+//evento
+document.addEventListener("DOMContentLoaded", llamarAPI);
+
+
+//funciones
+function llamarAPI(){
+  fetch(url)
+  .then(resp => resp.json())
+  .then((data) => mostrarHTML(data)) 
+  }
+function mostrarHTML(datos){
+  datos.forEach(item => {
+    const row = document.createElement('tr');
+    // ` alt+96
+    row.innerHTML = `
+    <td>${item.name}</td>
+    <td>${item.email}</td>
+    <td>${item.address.city}</td>
+    `
+    respuesta.appendChild(row)
+  });
+}
+
+
+
+
+
 navToggle.addEventListener("click", () => {
   navMenu.classList.toggle("nav-menu_visible");
 
